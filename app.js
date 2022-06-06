4633463af4d0b923d22108759e9d0297abbd7f4e
emiliano <earancio616@gmail.com>
2022-06-05 22:36:39 -0300

    SDFDFSFDSJSDFJDF

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const app = express();
 
 //motor de plantillas
 app.set('view engine', 'ejs')
-app.set('trust proxy', 'loopback')
 //usar carpeta para static files
 app.use("/public", express.static(__dirname + '/public'));
 
