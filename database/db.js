e7d7995a9df53ad611ba12650733059209f160aa
emiliano <earancio616@gmail.com>
2022-06-19 17:10:22 -0300

    serverless mysql config

--- database/db.js
+++ database/db.js
@@ -1,17 +1,12 @@
-const mysql = require('serverless-mysql')
+const db = require('serverless-mysql')()
 
-const db = mysql.createConnection({
+db.config({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
 })
 
-db.connect( (e) => {
-    if(e) return console.log(e)
-    console.log('DB connected.')
-})
-
-db.on('error', () => db.connect())
+console.log('DB connected')
 
 module.exports = db
